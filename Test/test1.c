8e4c0fb1a6f4ba9c6a4148354caaed9ccaf429f3
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2023-02-22 22:18:38 +0530

    cmd args test

--- Test/test1.c
+++ Test/test1.c
@@ -1,21 +1,23 @@
 #include<stdio.h> 
 #include<string.h>
 #include<stdlib.h>
+#include<assert.h>
 
-int main(){
-    FILE *f ; 
-    f = fopen("/Users/milindrao/Desktop/college/3-2/OS/Code/Test/test.txt" , "w"); 
-    if(f == NULL){
-        printf("Error"); 
-        return 0; 
+int main(int argc , char* argsv[]){
+    int arr[5] = {1 , 2 , 3 , 4 , 5}; 
+    FILE* f; 
+    f = fopen(argsv[1] , "w"); 
+    for(int i = 0 ; i < 5 ; i++){
+        fprintf(f , "%d " , arr[i]); 
     }
-    int n ; 
-    scanf("%d" , &n);
-    fprintf(f , "%d" , n); 
     fclose(f); 
-    int  num;  
-    f = fopen("/Users/milindrao/Desktop/college/3-2/OS/Code/Test/test.txt" , "r"); 
-    fscanf(f , "%d" , &num);
-    fclose(f); 
-    printf("Found : %d" , num);  
+    f = fopen(argsv[1] , "r"); 
+    int output[5] = {0 , 0 , 0 , 0 , 0}; 
+    for(int i = 0 ; i < 5 ; i++){
+        fscanf(f , "%d" , &output[i]);  
+    }
+    for(int i = 0 ; i < 5 ; i++){
+        printf("%d " , output[i]); 
+    }
+    return 0; 
 }
---
+++ Test/test2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argsc , char* argsv[]){
+    int temp = atoi(argsv[1]); 
+    printf("%d \n" , temp); 
+    printf("%d" , argsc); 
+    return 0; 
+}

ddd04f125c1778277bc91d67acf12656f61e2e0a
raomilind04 <72732775+raomilind04@users.noreply.github.com>
2023-03-04 23:13:51 +0530

    random

--- Test/test2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<stdlib.h>
-
-int main(int argsc , char* argsv[]){
-    int temp = atoi(argsv[1]); 
-    printf("%d \n" , temp); 
-    printf("%d" , argsc); 
-    return 0; 
-}
--- Test/testc.c
+++
@@ -1,7 +0,0 @@
-#include <sys/types.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <time.h>
